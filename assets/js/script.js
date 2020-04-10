de4f8073dd982005b1c91e3e9c204695a7e83cbb
amybru <amy.bru39@gmail.com>
2020-04-10 19:42:09 +0000

    Formatting issue fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,23 @@
-$(document).ready(function() {
-   
-//For scroll to top button located in the footer
-  mybutton = document.getElementById("toTopBtn");
+$(document).ready(function () {
 
-window.onscroll = function() {scrollFunction()};
+	//For scroll to top button located in the footer
+	mybutton = document.getElementById("toTopBtn");
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("toTopBtn").style.display = "block";
-  } else {
-    toTopBtn.style.display = "none";
-  
-  }
-}
+	window.onscroll = function () {
+		scrollFunction()
+	};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.getElementById("toTopBtn").style.display = "block";
+		} else {
+			toTopBtn.style.display = "none";
+
+		}
+	}
 });
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; //Safari browser
-  document.documentElement.scrollTop = 0;  //Chrome, Firefox, IE browsers
+	document.body.scrollTop = 0; //Safari browser
+	document.documentElement.scrollTop = 0; //Chrome, Firefox, IE browsers
 }
-
-
-
-
